7b325c1f18868469f6f7b5c37e8f50ff9e51c0be
AboubakarJay <aboubakarjay2@hotmail.com>
2025-02-03 14:58:30 +0100

    3 feb.

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-var bigYellowCircle = document.querySelector("#bigYellowCircle");
-var blueSquare = document.querySelector("#blueSquare");
-var greenPentagon = document.querySelector("#greenPentagon");
+let bigYellowCircle = document.querySelector("#bigYellowCircle");
+let blueSquare = document.querySelector("#blueSquare");
+let greenPentagon = document.querySelector("#greenPentagon");
 
-function setTranslate(xPos, yPos, el) {
-  el.style.transform = `translate3d(${xPos}px,
-    ${yPos}px,
-    0)`;
+function setTransform(xPos, yPos, el) {
+  el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
 }
+
+window.addEventListener("DOMContentLoaded", scrollLoop);
+
+let xScrollPosition;
+let yScrollPosition;
 
 function scrollLoop() {
   xScrollPosition = window.scrollX;
   yScrollPosition = window.scrollY;
-  setTranslate(0, yScrollPosition * -0.2, bigYellowCircle);
-  setTranslate(0, yScrollPosition * -1.5, blueSquare);
-  setTranslate(0, yScrollPosition * 0.5, greenPentagon);
+
+  setTransform(0, yScrollPosition * -0.2, bigYellowCircle);
+  setTransform(0, yScrollPosition * -1.5, blueSquare);
+  setTransform(0, yScrollPosition * 0.6, greenPentagon);
+
   requestAnimationFrame(scrollLoop);
 }
-
-window.addEventListener("DOMContentLoaded", scrollLoop, false);
